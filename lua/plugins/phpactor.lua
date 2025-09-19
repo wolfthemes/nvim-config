af85ee4d9ef861cd12883e08341b83c13e5ad31b
Constantin Saguin <wolfthemes14@gmail.com>
2025-09-19 10:07:35 +0200

    fix: phpactor config file

--- lua/plugins/phpactor.lua
+++ lua/plugins/phpactor.lua
@@ -1,15 +1,12 @@
 return {
   "neovim/nvim-lspconfig",
   config = function()
-    local lspconfig = require("lspconfig")
-
-    lspconfig.phpactor.setup({
+    vim.lsp.config.phpactor = {
       cmd = { vim.fn.stdpath("data") .. "/mason/bin/phpactor", "language-server" },
       on_attach = function(client, bufnr)
         local opts = { noremap = true, silent = true, buffer = bufnr }
         vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
       end,
-    })
+    }
   end,
 }
-
